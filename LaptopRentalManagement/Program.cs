54bce1c91791c97ac03755997a1cc9043d07dc34
Đặng Mai Anh Tú <tudmase182951@fpt.edu.vn>
2025-07-09 19:37:25 +0700

    Re init

--- LaptopRentalManagement/Program.cs
+++ LaptopRentalManagement/Program.cs
@@ -1,78 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-using LaptopRentalManagement.DAL.Context;
-using LaptopRentalManagement.DAL.Interfaces;
-using LaptopRentalManagement.BLL.Interfaces;
-using LaptopRentalManagement.BLL.Mappings;
-using LaptopRentalManagement.Hubs;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
-// Add Entity Framework
-builder.Services.AddDbContext<LaptopRentalDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Add Authentication & Authorization
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]!)),
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ClockSkew = TimeSpan.Zero
-        };
-        
-        // For SignalR
-        options.Events = new JwtBearerEvents
-        {
-            OnMessageReceived = context =>
-            {
-                var accessToken = context.Request.Query["access_token"];
-                var path = context.HttpContext.Request.Path;
-                if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/notificationHub"))
-                {
-                    context.Token = accessToken;
-                }
-                return Task.CompletedTask;
-            }
-        };
-    });
-
-builder.Services.AddAuthorization();
-
-// Add AutoMapper
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-// Add SignalR
-builder.Services.AddSignalR();
-
-// Add CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigins", policy =>
-    {
-        policy.WithOrigins("http://localhost:3000", "https://localhost:3001") // Add your frontend URLs
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
-    });
-});
-
-// Register repositories and services
-// Note: Actual implementations need to be created
-// builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-// builder.Services.AddScoped<IAccountService, AccountService>();
-// builder.Services.AddScoped<ILaptopService, LaptopService>();
-// builder.Services.AddScoped<IOrderService, OrderService>();
-// builder.Services.AddScoped<INotificationService, NotificationService>();
 
 var app = builder.Build();
 
@@ -87,18 +16,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-// Use CORS
-app.UseCors("AllowSpecificOrigins");
-
 app.UseRouting();
 
-// Authentication and Authorization
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
-
-// Map SignalR Hub
-app.MapHub<NotificationHub>("/notificationHub");
 
 app.Run();
