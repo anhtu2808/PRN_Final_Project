71fe1d9351b6705d7e8a21ebdbe723ba8b3e6976
Longnee <longthse184113@fpt.edu.vn>
2025-07-11 19:11:11 +0700

    model

---
+++ LaptopRentalManagement.BLL/Models/Dashboard/TopLaptop.cs
@@ -0,0 +1,10 @@
+﻿namespace LaptopRentalManagement.Models.Dashboard
+{
+    public class TopLaptop
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int RentCount { get; set; }
+        public decimal Revenue { get; set; }
+    }
+}
