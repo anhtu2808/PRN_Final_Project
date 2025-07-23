82a7f02126e4c64b3bd02486710343e0e6550d04
Đặng Mai Anh Tú <tudmase182951@fpt.edu.vn>
2025-07-23 15:00:10 +0700

    Add simalar laptop

--- LaptopRentalManagement/Pages/Laptops/Details.cshtml.cs
+++ LaptopRentalManagement/Pages/Laptops/Details.cshtml.cs
@@ -1,5 +1,6 @@
 using LaptopRentalManagement.BLL.DTOs.Response;
 using LaptopRentalManagement.BLL.Interfaces;
+using LaptopRentalManagement.Model.DTOs.Request;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,6 +9,7 @@ namespace LaptopRentalManagement.Pages.Laptops
     public class DetailsModel : PageModel
     {
         public LaptopResponse? Laptop { get; set; } = new LaptopResponse();
+        public IList<LaptopResponse> SimilarLaptops { get; set; } = new List<LaptopResponse>();
         private readonly ILaptopService _laptopService;
 
         public DetailsModel(ILaptopService laptopService)
@@ -22,6 +24,17 @@ namespace LaptopRentalManagement.Pages.Laptops
             {
                 return NotFound();
             }
+
+            // Load similar laptops based on the same category
+            var filter = new LaptopFilter()
+            {
+                CategoryIds = new List<int> { Laptop.Categories.FirstOrDefault()?.CategoryId ?? 0 },
+            };
+            SimilarLaptops = await _laptopService.GetAllAsync(filter);
+            SimilarLaptops = SimilarLaptops
+                .Where(x => x.LaptopId != Laptop.LaptopId) // Exclude the current laptop
+                .Take(4) // Limit to 4 similar laptops
+                .ToList();
 
             return Page();
         }
