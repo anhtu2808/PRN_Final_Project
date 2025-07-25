cfb2ebf2a03328a971d85c6aa9c2a73559f4eb16
haocp <haocpse184107@fpt.edu.vn>
2025-07-25 19:55:39 +0700

    add orderlog and orderlogImg

---
+++ LaptopRentalManagement.DAL/Entities/OrderLogImg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopRentalManagement.DAL.Entities
+{
+    public class OrderLogImg
+    {
+        public int Id { get; set; }
+
+        public int OrderLogId { get; set; }
+
+        public string ImgUrl { get; set; } = null!;
+
+        public virtual OrderLog OrderLog { get; set; } = null!;
+    }
+}
--- LaptopRentalManagement/Pages/User/Rental-order/Index.cshtml.cs
+++ LaptopRentalManagement/Pages/User/Rental-order/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using LaptopRentalManagement.BLL.DTOs.Response;
+using LaptopRentalManagement.BLL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,23 @@ namespace LaptopRentalManagement.Pages.User.Rental_order
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IOrderService _orderService;
+
+        public IndexModel(IOrderService orderService)
         {
+            _orderService = orderService;
+        }
+
+        public OrderResponse? Order { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int orderId)
+        {
+            Order = await _orderService.GetByIdAsync(orderId);
+            if (Order == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
