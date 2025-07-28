0aa1384268065389d8153c20152f387013c0bdc2
haocp <haocpse184107@fpt.edu.vn>
2025-07-29 03:09:32 +0700

    add UI order detail for manager

---
+++ LaptopRentalManagement/Pages/Manage/Orders/Detail.cshtml.cs
@@ -0,0 +1,111 @@
+﻿using LaptopRentalManagement.BLL.DTOs.Request;
+using LaptopRentalManagement.BLL.DTOs.Response;
+using LaptopRentalManagement.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace LaptopRentalManagement.Pages.Manage.Orders
+{
+    public class DetailModel : PageModel
+    {
+		private readonly IOrderService _orderService;
+		private readonly IOrderLogService _orderLogService;
+		private readonly ITicketService _ticketService;
+
+		public DetailModel(
+		IOrderService orderService,
+		IOrderLogService orderLogService,
+		ITicketService ticketService)
+		{
+			_orderService = orderService;
+			_orderLogService = orderLogService;
+			_ticketService = ticketService;
+		}
+
+		public OrderResponse? Order { get; set; }
+		public IList<OrderLogResponse> Logs = new List<OrderLogResponse>();
+		public IList<TicketResponse> Tickets { get; set; } = new List<TicketResponse>();
+
+		public async Task<IActionResult> OnGetAsync(int orderId)
+		{
+			var userIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? User.FindFirstValue("AccountId");
+			if (!int.TryParse(userIdClaim, out var userId))
+			{
+				TempData["Error"] = "Please login to continue";
+				return RedirectToPage("/Account/Login");
+			}
+			Order = await _orderService.GetByIdAsync(orderId);
+			Logs = await _orderLogService.GetByOrderIdAsync(orderId);
+			Tickets = await _ticketService.GetAllByOrderIdAsync(orderId);
+			if (Order == null)
+			{
+				return NotFound();
+			}
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostApproveAsync(int orderId, int id)
+		{
+			await _orderService.ApproveAsync(orderId);
+			TempData["Success"] = $"Đã duyệt đơn #{orderId}.";
+			return RedirectToPage("/User/Lease-Order/Detail", new { id });
+		}
+
+		public async Task<IActionResult> OnPostRejectAsync(int orderId, int id)
+		{
+			await _orderService.RejectAsync(orderId);
+			TempData["Success"] = $"Đã từ chối đơn #{orderId}.";
+			return RedirectToPage("/User/Lease-Order/Detail", new { id });
+		}
+
+		public async Task<IActionResult> OnPostConfirmReturnAsync(int orderId, int id)
+		{
+			await _orderService.ConfirmReturn(orderId);
+			TempData["Success"] = $"Đơn #{orderId} đã được xác nhận là đã trả.";
+			return RedirectToPage("/User/Lease-Order/Detail", new { id });
+		}
+
+		public async Task<IActionResult> OnPostDeliveringAsync(int orderId, int id)
+		{
+			await _orderService.SetStatusAsync(new()
+			{
+				OrderId = orderId,
+				NewStatus = "Delivering"
+			});
+			TempData["Success"] = $"Đơn #{orderId} đang giao hàng.";
+			return RedirectToPage("/User/Lease-Order/Detail", new { id });
+		}
+
+		public async Task<IActionResult> OnPostDeliveredAsync(int orderId, int id)
+		{
+			await _orderService.SetStatusAsync(new()
+			{
+				OrderId = orderId,
+				NewStatus = "Renting"
+			});
+			TempData["Success"] = $"Đơn #{orderId} đã giao thành công.";
+			return RedirectToPage("/User/Lease-Order/Detail", new { id });
+		}
+
+		[BindProperty]
+		public List<IFormFile> Images { get; set; } = new();
+
+		public async Task<IActionResult> OnPostDeliveredFailAsync(int orderId, int id, string reason)
+		{
+			var request = new OrderLogRequest
+			{
+				OrderId = orderId,
+				NewStatus = "DeliveringFail",
+				Reason = reason,
+				Forms = Images
+			};
+
+			await _orderService.SetStatusAsync(request);
+
+			TempData["Warning"] = $"Giao hàng đơn #{orderId} thất bại. Trạng thái quay về 'Delivering'.";
+
+			return RedirectToPage("/User/Lease-Order/Detail", new { id });
+		}
+	}
+}
