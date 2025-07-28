ce7ce0713e68b11fc5a2437a8a71c82d871852c8
Uông Đình Dũng <dungminhhao@gmail.com>
2025-07-28 19:45:34 +0700

    Update Account.cs

--- LaptopRentalManagement.DAL/Entities/Account.cs
+++ LaptopRentalManagement.DAL/Entities/Account.cs
@@ -5,24 +5,10 @@ namespace LaptopRentalManagement.DAL.Entities;
 public partial class Account
 {
     public int AccountId { get; set; }
-
-    [Required(ErrorMessage = "Email is required")]
-    [EmailAddress(ErrorMessage = "Please input valid email")]
-    [Display(Name = "Email")]
     public string Email { get; set; } = null!;
-
-    [Required(ErrorMessage = "Password is required")]
-    [StringLength(100, MinimumLength = 6, ErrorMessage = "Length must be greater than 6")]
-    [DataType(DataType.Password)]
-    [Display(Name = "Password")]
     public string PasswordHash { get; set; } = null!;
-
     public string Role { get; set; } = null!;
-    [Required(ErrorMessage = "Full name is required")]
-    [StringLength(100, MinimumLength = 2, ErrorMessage = "Length must be greater than 2 and lower than 100")]
-    [Display(Name = "Full Name")]
     public string? Name { get; set; }
-
     public DateTime CreatedAt { get; set; }
 
     public virtual ICollection<Notification> Notifications { get; set; } = new List<Notification>();
